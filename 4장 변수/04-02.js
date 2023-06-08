68355c49fe6d754564e5a59786625ba7fe22c569
stop-zero <jungjy0097@naver.com>
2023-06-08 23:57:54 +0900

    Docs: study 4장 변수

---
+++ 4장 변수/04-02.js
@@ -0,0 +1,15 @@
+// 변수에 여러 개의 값을 저장하는 방법
+//  => 배열, 객체를 사용하여 그룹화
+
+const userId = 1;
+const userName = 'Lee';
+
+const user = { id: 1, name: 'Lee' };
+
+const users = [
+  { id: 1, name: 'Lee' },
+  { id: 2, name: 'Kim' },
+];
+
+console.log(result);
+let result;
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ModernJS_DeepDive
 📕 ModernJS_DeepDive Study
+
+#### [4장 변수](https://miunoribird.tistory.com/71)
